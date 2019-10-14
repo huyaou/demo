d10cb0e4d0779d0ed06b98a947264ec2bc8967dd
huyaou <1009003540@qq.com>
2019-10-14 11:26:02 +0800

    ceshilei

---
+++ src/main/java/com/example/demo/TestDEMO.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+/**
+ * @program: demo
+ * @description: ceshi
+ * @author: huyaou
+ * @create: 2019-10-14 11:21
+ */
+public class TestDEMO {
+    private Integer testId;
+
+
+    public Integer getTestId() {
+        return testId;
+    }
+
+    public void setTestId(Integer testId) {
+        this.testId = testId;
+    }
+}
